ba0383b0b8cfd2e49231f7d4dfd34145c3cc8e9f
edy17 <edy.diehl@outlook.fr>
2024-06-17 11:35:46 +0200

    Airflow orchestration

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Predicting flight delays
+# Predicting flight delays at scale
+
+The purpose of this project is to predict flight delays due to significant weather conditions from a big data processing application.
+Apache Spark and Scala are used for data processing and modeling, Apache Airflow is used for workflow automation,
+Amazon S3 and Delta lake for storage, and PowerBI is used for visualization. Flight data are provided by the
+[Bureau of Transportation Statistics](https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGJ&QO_fu146_anzr=b0-gvzr)
+that compiles, analyzes and publishes information on transportation systems in the USA. In particular, they provide a
+[delay cause Definition](https://www.bts.gov/topics/airlines-and-airports/understanding-reporting-causes-flight-delays-and-cancellations),
+and [summary statistics on the weather's share](https://www.transtats.bts.gov/ot_delay/ot_delaycause1.asp?6B2r=G&20=E).
+Weather data are provided by the [NCEI](https://www.ncei.noaa.gov/),
+a US government agency that manages one of the world's largest archives of atmospheric,
+coastal, geophysical, and oceanic data.
+We use [Local Climatological Data](https://www1.ncdc.noaa.gov/pub/data/cdo/documentation/LCD_documentation.pdf)
+that provide a monthly synopses of climatic values for individual weather station.
+
+### Data visualization
+
+Figures and PowerBI analysis report are available on the [dedicated web page](https://edy17.github.io/predicting-flight-delays/report.html).
+
+### Set up project
+
+Copy the project files to the `$AIRFLOW_HOME` folder or set this folder as Airflow's home directory by running:
+
+```bash
+export AIRFLOW_HOME=<current root repository>
+```
+
+### Building project
+
+Using [Sbt](https://www.scala-sbt.org/), run in the current root repository:
+```bash
+sbt clean package
+```
+
+### Running Airflow pipeline from REST API
+
+- Requirements:
+    - Apache Airflow cluster and his webserver available at `https://example.com`
+    - [Basic authentication](https://airflow.apache.org/docs/apache-airflow/stable/stable-rest-api-ref.html#section/Authentication) enabled
+    - [Apache Spark provider](https://airflow.apache.org/docs/apache-airflow-providers-apache-spark/stable/index.html#installation)
+    - [Spark connection](https://airflow.apache.org/docs/apache-airflow-providers-apache-spark/stable/connections/spark-connect.html)
+      configured with id `spark_cluster`
+
+Once all requirements are met, DAG can be run using:
+
+```bash
+curl --location 'https://example.com/api/v1/dags/flight_delays_modeling_dag/dagRuns' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--user "username:password" \
+--data '{"dag_run_id": "example_id"}'
+```
---
+++ dags/flight_delays_dag.py
@@ -0,0 +1,270 @@
+import datetime as dt
+import os
+import re
+from io import BytesIO
+from typing import Sequence
+from zipfile import ZipFile
+
+import pandas as pd
+import requests
+from airflow.models import Variable
+from airflow.models.dag import DAG
+from airflow.models.param import ParamsDict
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+
+class CustomPythonOperator(PythonOperator):
+    template_fields: Sequence[str] = ("processed_year",)
+
+    def __init__(self, processed_year: str, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.processed_year = processed_year
+
+    def execute(self, context) -> None:
+        if not bool(context.get("params")):
+            context.update({"params": ParamsDict({"year": self.processed_year})})
+        super().execute(context)
+
+
+class CustomSparkSubmitOperator(SparkSubmitOperator):
+    template_fields: Sequence[str] = ("processed_year", "application_args",)
+
+    def __init__(self, processed_year: str, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.processed_year = processed_year
+
+    def execute(self, context) -> None:
+        if not bool(context.get("params")):
+            context.update({"params": ParamsDict({"year": self.processed_year})})
+        super().execute(context)
+
+
+with DAG("flight_delays_modeling_dag",
+         schedule=None,
+         start_date=dt.datetime(2024, 6, 1),
+         is_paused_upon_creation=False,
+         ) as dag:
+    bts_dataset_url = "https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_"
+    ncei_dataset_url = "https://www.ncei.noaa.gov/data/local-climatological-data/access"
+    station_per_airport_file = f"{os.environ['AIRFLOW_HOME']}/wban_airport_timezone.csv"
+    local_data_location = f"{os.environ['AIRFLOW_HOME']}/data/task"
+    raw_data_location = f"file://{local_data_location}"
+    lakehouse_location = f"file://{os.environ['AIRFLOW_HOME']}/data/spark-warehouse"
+    spark_jars_packages = "io.delta:delta-core_2.12:2.4.0"
+    application_path = f"file://{os.environ['AIRFLOW_HOME']}/target/scala-2.12/predicting-flight-delays_2.12-0.1.0-SNAPSHOT.jar"
+    spark_conn_id = "spark_cluster"
+
+
+    def get_processed_year():
+        max_year = 2024
+        try:
+            year = int(Variable.get("year")) + 1  # Value of current run
+        except:
+            year = 2012  # Value of first run
+        if year > max_year:
+            raise Exception("All years are already processed.")
+        Variable.set(key="year", value=year)
+        return year
+
+
+    def flight_data_ingestion(params):
+        print("::group:: flight_data_ingestion")
+        year = params["year"]
+        save_path = f"{local_data_location}/{year}/flights"
+        processed_months = range(1, 13)
+        for month in processed_months:
+            url = f"{bts_dataset_url}{year}_{month}.zip"
+            response = requests.get(url, stream=True, allow_redirects=True)
+            total_size = int(response.headers.get("content-length", 0))
+            chunk_size = 1024
+            buffer = BytesIO()
+            for data in response.iter_content(chunk_size=chunk_size):
+                buffer.write(data)
+            with ZipFile(buffer) as zip_file:
+                files = [zip_file.extract(file, save_path) for file in zip_file.namelist() if file.endswith(".csv")]
+            if total_size != 0 and not os.path.isfile(files[0]):
+                raise Exception(f"Could not download zip file : {url}")
+            print(f"{files[0]} file download done")
+        print("::endgroup::")
+
+
+    def weather_data_ingestion(params):
+        print("::group:: weather_data_ingestion")
+        year = params["year"]
+        url = f"{ncei_dataset_url}/{year}"
+        os.makedirs(f"{local_data_location}/{year}/weather", exist_ok=True)
+        response = requests.get(url, stream=True, allow_redirects=True)
+        station_files = re.findall("href=\"(.*?csv)\"", response.text)
+        station_ids = pd.read_csv(station_per_airport_file)["WBAN"].astype(str).tolist()
+        known_stations = list(map(lambda x: (x[len(x) - 5 - 4:len(x) - 4], x), station_files))
+        known_stations = list(filter(lambda x: x[0] in station_ids, known_stations))
+        known_stations = list(map(lambda x: x[1], known_stations))
+        for file_name in known_stations:
+            weather_url = f"{ncei_dataset_url}/{year}/{file_name}"
+            filepath = f"{local_data_location}/{year}/weather/{file_name}"
+            response = requests.get(weather_url, stream=True, allow_redirects=True)
+            total_size = int(response.headers.get("content-length", 0))
+            chunk_size = 1024
+            with open(filepath, "wb") as file:
+                for data in response.iter_content(chunk_size):
+                    file.write(data)
+            if total_size != 0 and not os.path.isfile(filepath):
+                raise RuntimeError(f"Could not download file : {filepath}")
+            print(f"{file_name} file download done")
+        print("::endgroup::")
+
+
+    get_current_year = PythonOperator(
+        task_id="get_processed_year",
+        python_callable=get_processed_year)
+
+    current_year = "{{ ti.xcom_pull(task_ids='get_processed_year', dag_id='flight_delays_modeling_dag', key='return_value') }}"
+    year_param = "{{ params['year'] }}"
+
+    download_flights = CustomPythonOperator(
+        task_id="flight_data_ingestion",
+        python_callable=flight_data_ingestion,
+        processed_year=current_year)
+
+    download_weather = CustomPythonOperator(
+        task_id="weather_data_ingestion",
+        python_callable=weather_data_ingestion,
+        processed_year=current_year)
+
+    flights_preprocessing = CustomSparkSubmitOperator(
+        task_id="flight_data_preprocessing",
+        name="flight_data_preprocessing",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.FlightsPreprocessing",
+        application=application_path,
+        application_args=[f"file://{station_per_airport_file}",
+                          f"{raw_data_location}/{year_param}/flights",
+                          f"{lakehouse_location}/{year_param}/flights"],
+        processed_year=current_year)
+
+    weather_preprocessing = CustomSparkSubmitOperator(
+        task_id="weather_data_preprocessing",
+        name="weather_data_preprocessing",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.WeatherPreprocessing",
+        application=application_path,
+        application_args=[f"file://{station_per_airport_file}",
+                          f"{raw_data_location}/{year_param}/weather",
+                          f"{lakehouse_location}/{year_param}/raw_weather"],
+        processed_year=current_year)
+
+    weather_resampling = CustomSparkSubmitOperator(
+        task_id="weather_data_resampling",
+        name="weather_data_resampling",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.WeatherResampling",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/raw_weather",
+            f"{lakehouse_location}/{year_param}/weather"],
+        processed_year=current_year)
+
+    weather_interpolation = CustomSparkSubmitOperator(
+        task_id="weather_data_interpolation",
+        name="weather_data_interpolation",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.WeatherInterpolation",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/weather",
+            f"{lakehouse_location}/{year_param}/interpolated_weather"],
+        processed_year=current_year)
+
+    weather_assembling = CustomSparkSubmitOperator(
+        task_id="weather_data_assembling",
+        name="weather_data_assembling",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.WeatherAssembling",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/interpolated_weather",
+            f"{lakehouse_location}/{year_param}/org_vectorized_weather",
+            f"{lakehouse_location}/{year_param}/dest_vectorized_weather"],
+        processed_year=current_year)
+
+    origin_airports_processing = CustomSparkSubmitOperator(
+        task_id="origin_airports_processing",
+        name="origin_airports_processing",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.OriginAirportsProcessing",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/flights",
+            f"{lakehouse_location}/{year_param}/org_vectorized_weather",
+            f"{lakehouse_location}/{year_param}/org_delay"],
+        processed_year=current_year)
+
+    destination_airports_processing = CustomSparkSubmitOperator(
+        task_id="destination_airports_processing",
+        name="destination_airports_processing",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.DestinationAirportsProcessing",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/org_delay",
+            f"{lakehouse_location}/{year_param}/dest_vectorized_weather",
+            f"{lakehouse_location}/{year_param}/org_dest_delay"],
+        processed_year=current_year)
+
+    vector_assembling = CustomSparkSubmitOperator(
+        task_id="vector_assembling",
+        name="vector_assembling",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.VectorAssembling",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/org_dest_delay",
+            f"{lakehouse_location}/{year_param}/delay"],
+        processed_year=current_year)
+
+    under_sampling = CustomSparkSubmitOperator(
+        task_id="data_under_sampling",
+        name="data_under_sampling",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.UnderSampling",
+        application=application_path,
+        application_args=[
+            f"{lakehouse_location}/{year_param}/delay",
+            f"{lakehouse_location}/train_delay",
+            f"{lakehouse_location}/test_delay"],
+        processed_year=current_year)
+
+    modeling = CustomSparkSubmitOperator(
+        task_id="model_training_and_evaluation",
+        name="model_training_and_evaluation",
+        packages=spark_jars_packages,
+        conn_id=spark_conn_id,
+        java_class="org.diehl.workflow.Modeling",
+        application=application_path,
+        application_args=[f"{lakehouse_location}/train_delay",
+                          f"{lakehouse_location}/test_delay",
+                          f"{lakehouse_location}/{year_param}/train_roc",
+                          f"{lakehouse_location}/{year_param}/test_roc"],
+        processed_year=current_year)
+
+    get_current_year >> [download_flights, download_weather]
+    download_flights >> flights_preprocessing
+    download_weather >> weather_preprocessing
+    weather_preprocessing >> weather_resampling
+    weather_resampling >> weather_interpolation
+    weather_interpolation >> weather_assembling
+    [flights_preprocessing, weather_assembling] >> origin_airports_processing
+    origin_airports_processing >> destination_airports_processing
+    destination_airports_processing >> vector_assembling
+    vector_assembling >> under_sampling
+    under_sampling >> modeling
